03b4f5dd1ef47b3633ef5564209c36d1c207a498
Wliky <June27November21@gmail.com>
2026-06-24 18:23:33 +0800

    Revert "## update my bolg"

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -50,11 +50,11 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "晴天",
-				artist: "晴天 / 周杰伦 / Jay",
-				url: "/assets/music/晴天 - 周杰伦.flac",
-				cover: "/assets/music/cover/晴天.png",
-				lrc: "",
+                name: "使一颗心免于哀伤",
+                artist: "知更鸟 / HOYO-MiX / Chevy",
+                url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
+                cover: "/assets/music/cover/109951169585655912.webp",
+                lrc: "",
 			},
 		],
 	},
