278f5aeca904fa8deff1c89a27ae0bab21f591e2
Wliky <June27November21@gmail.com>
2026-08-10 01:21:04 +0800

    Revert "## update my bolg"

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -2,7 +2,7 @@ import type { BackgroundWallpaperConfig } from "@/types/backgroundWallpaper";
 
 export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	// 壁纸模式："banner" 横幅壁纸，"fullscreen" 全屏壁纸，"overlay" 全屏透明，"none" 纯色背景无壁纸
-	mode: "banner",
+	mode: "overlay",
 	// 是否启用背景视频播放，配置后将在导航栏显示视频播放按钮
 	playerEnable: true,
 	/**
@@ -152,9 +152,9 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 壁纸透明度
 		opacity: 0.8,
 		// 背景模糊度
-		blur: 10,
+		blur: 1,
 		// 卡片透明度，0-1之间，值越小越透明
-		cardOpacity: 0.5,
+		cardOpacity: 0.3,
 	},
 	// 全屏壁纸模式特有配置
 	fullscreen: {
